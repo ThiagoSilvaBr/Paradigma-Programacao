a31d6c05651238d371c5630ba10ba5f80d38ac19
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2026-08-04 21:35:35 -0300

    feat: add input de texto

--- Aula000/AtividadeEmRust.rs
+++ Aula000/AtividadeEmRust.rs
@@ -1,5 +1,13 @@
+use std::io;
+
 fn main() {
-    let number = 7;
+    let mut input = String::new();
+    println!("Digite um numero para ver sua tabuada");
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    let number: i32 = input.trim().parse().unwrap();
+    
     println!("{}", number);
 
     for i in 1..=10{
